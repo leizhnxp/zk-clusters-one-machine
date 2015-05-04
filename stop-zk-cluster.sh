a10f1bec5f2a2cff6e63a4a3e5951c7f41556ace
zhenhua.lei <zhenhua.lei@alibaba-inc.com>
2015-05-04 17:49:21 +0800

    first import

---
+++ stop-zk-cluster.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo /opt/zookeeper-3.4.6/bin/zkServer.sh stop /home/vagrant/zk-multi-server-single-machine/zoo.cfg.2191
+sudo /opt/zookeeper-3.4.6/bin/zkServer.sh stop /home/vagrant/zk-multi-server-single-machine/zoo.cfg.2192
+sudo /opt/zookeeper-3.4.6/bin/zkServer.sh stop /home/vagrant/zk-multi-server-single-machine/zoo.cfg.2193
