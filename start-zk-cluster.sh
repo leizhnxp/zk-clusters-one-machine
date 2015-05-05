74e3c21520139f5c9b5b66455d43e38f9f47376d
leizhnxp <leizhnxp@gmail.com>
2015-05-05 07:19:31 +0000

    update for unrename

--- start-zk-cluster.sh
+++ start-zk-cluster.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/zk-multi-server-single-machine/data/2191
-mkdir -p ~/zk-multi-server-single-machine/data/2192
-mkdir -p ~/zk-multi-server-single-machine/data/2193
+mkdir -p ~/zk-clusters-one-machine/data/2191
+mkdir -p ~/zk-clusters-one-machine/data/2192
+mkdir -p ~/zk-clusters-one-machine/data/2193
 
-echo "1" > ~/zk-multi-server-single-machine/data/2191/myid
-echo "2" > ~/zk-multi-server-single-machine/data/2192/myid
-echo "3" > ~/zk-multi-server-single-machine/data/2193/myid
+echo "1" > ~/zk-clusters-one-machine/data/2191/myid
+echo "2" > ~/zk-clusters-one-machine/data/2192/myid
+echo "3" > ~/zk-clusters-one-machine/data/2193/myid
 
-/opt/zookeeper-3.4.6/bin/zkServer.sh start ~/zk-multi-server-single-machine/zoo.cfg.2191
-/opt/zookeeper-3.4.6/bin/zkServer.sh start ~/zk-multi-server-single-machine/zoo.cfg.2192
-/opt/zookeeper-3.4.6/bin/zkServer.sh start ~/zk-multi-server-single-machine/zoo.cfg.2193
+/opt/zookeeper-3.4.6/bin/zkServer.sh start ~/zk-clusters-one-machine/zoo.cfg.2191
+/opt/zookeeper-3.4.6/bin/zkServer.sh start ~/zk-clusters-one-machine/zoo.cfg.2192
+/opt/zookeeper-3.4.6/bin/zkServer.sh start ~/zk-clusters-one-machine/zoo.cfg.2193
--- status-zk-cluster.sh
+++ status-zk-cluster.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo /opt/zookeeper-3.4.6/bin/zkServer.sh status /home/vagrant/zk-multi-server-single-machine/zoo.cfg.2191
-sudo /opt/zookeeper-3.4.6/bin/zkServer.sh status /home/vagrant/zk-multi-server-single-machine/zoo.cfg.2192
-sudo /opt/zookeeper-3.4.6/bin/zkServer.sh status /home/vagrant/zk-multi-server-single-machine/zoo.cfg.2193
+sudo /opt/zookeeper-3.4.6/bin/zkServer.sh status /home/vagrant/zk-clusters-one-machine/zoo.cfg.2191
+sudo /opt/zookeeper-3.4.6/bin/zkServer.sh status /home/vagrant/zk-clusters-one-machine/zoo.cfg.2192
+sudo /opt/zookeeper-3.4.6/bin/zkServer.sh status /home/vagrant/zk-clusters-one-machine/zoo.cfg.2193
