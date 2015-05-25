54d815295ef33cddb7a2ed581d92d1cd2d36b944
leizhnxp <leizhnxp@gmail.com>
2015-05-25 07:17:23 +0000

    add install zk script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /opt
+sudo wget http://mirror.bit.edu.cn/apache/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz -O zookeeper-3.4.6.tar.gz
+sudo tar xvf zookeeper-3.4.6.tar.gz
